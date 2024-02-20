90b48f67b8e8b565e91ede27f3b011d19588c188
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-02-20 13:04:42 +0330

    Acessing URL parameters

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,3 +1,10 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 export const Note = (props) => {
+  const { noteId } = useParams();
+  const note = useSelector((store) =>
+    store.notesSlice.noteList.find((note) => note.id == noteId)
+  );
+  console.log("***", note);
   return <>Note</>;
 };
