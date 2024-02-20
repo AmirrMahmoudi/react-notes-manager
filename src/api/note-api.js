7062f9cde3f9d17bafe8de36da909e7fbc6ac71f
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-02-20 15:28:12 +0330

    Update note

--- src/api/note-api.js
+++ src/api/note-api.js
@@ -4,18 +4,24 @@ const BASE_URL = "http://localhost:3200/notes";
 
 export class NoteAPI {
   static async create(formValues) {
-    return (await axios.post(`${BASE_URL}`, formValues)).data;
+    return this.formatId((await axios.post(`${BASE_URL}`, formValues)).data);
   }
   static async fetchAll() {
-    return (await axios.get(`${BASE_URL}`)).data;
-  }
-  static async fetchBtId(noteId) {
-    return (await axios.get(`${BASE_URL}/${noteId}`)).data;
+    return (await axios.get(`${BASE_URL}`)).data.map(this.formatId);
   }
   static async deleteById(noteId) {
-    return (await axios.delete`${BASE_URL}/${noteId}`()).data;
+    return (await axios.delete(`${BASE_URL}/${noteId}`)).data;
   }
-  static async updateById(note) {
-    return (await axios.patcht(`${BASE_URL}/${note.id}`, note)).data;
+  static async updateById(id, values) {
+    return this.formatId((await axios.patch(`${BASE_URL}/${id}`, values)).data);
+  }
+  static async fetchBtId(noteId) {
+    return this.formatId((await axios.get(`${BASE_URL}/${noteId}`)).data);
+  }
+  static formatId(note) {
+    return {
+      ...note,
+      id: note.id.toString(),
+    };
   }
 }
