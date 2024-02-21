22b48ced0d67b8f9c93ef55356089afbff6b4d45
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-02-21 13:20:13 +0330

    Signin form structure

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,5 +1,23 @@
 import s from "./style.module.css";
+import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
+import { Link } from "react-router-dom";
 
 export const Signin = () => {
-  return <div>Signin</div>;
+  const form = (
+    <div className={s.formContainer}>
+      <h2 className={s.title}>
+        Signin <br />
+        to access your team notes
+      </h2>
+      <form action="" className={s.formGroup}>
+        <input type="text" />
+        <input type="text" />
+        <ButtonPrimary className={s.button}>Sign in!</ButtonPrimary>
+        <span>
+          Don't have an account yet ? <Link to={"/signup"}>Signup</Link>
+        </span>
+      </form>
+    </div>
+  );
+  return <div>{form}</div>;
 };
