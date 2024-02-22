b609f7ef4c58cd18aa4e6995425115a81d97e39d
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-02-22 19:10:54 +0330

    Auth layout part-2

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -19,11 +19,9 @@ function AuthLayout({ children }) {
 
   return (
     <div className={s.root}>
-      <div className={s.leftSection}>
-        {header}
-        {children}
-      </div>
-      <div className={s.rightSection}>{background}</div>
+      {header}
+      <div className={s.leftSection}>{children}</div>
+      <div className={`${s.rightSection} d-none d-lg-flex`}>{background}</div>
     </div>
   );
 }
