dffadf598fa9ed820ab8ad8f1143641cb57902d3
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-02-25 16:50:10 +0330

    Display the user profil Selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { NoteAPI } from "api/note-api";
 import { Header } from "components/Header/Header";
 import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setNoteList } from "store/notes/notes-slice";
 
-import s from "./style.module.css";
 import { withAuthRequired } from "hoc/withAuthRequired";
+import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
 
+import s from "./style.module.css";
 export function App() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   async function fetchAllNotes() {
     const noteList = await NoteAPI.fetchAll();
     dispatch(setNoteList(noteList));
@@ -21,6 +23,12 @@ export function App() {
   return (
     <div>
       <Header />
+      <ButtonPrimary
+        className={s.buttonAdd}
+        onClick={() => navigate("/note/new")}
+      >
+        +
+      </ButtonPrimary>
       <div className={s.workspace}>
         <Outlet />
       </div>
--- src/components/ButtonPrimary/ButtonPrimary.jsx
+++ src/components/ButtonPrimary/ButtonPrimary.jsx
@@ -1,11 +1,17 @@
 import s from "./style.module.css";
-export const ButtonPrimary = ({ type, children, onClick, isDisabled }) => {
+export const ButtonPrimary = ({
+  className,
+  type,
+  children,
+  onClick,
+  isDisabled,
+}) => {
   return (
     <button
       disabled={isDisabled}
       onClick={onClick}
       type={type}
-      className={`btn btn-primary ${s.button}`}
+      className={`btn btn-primary ${s.button} ${className}`}
     >
       {children}
     </button>
